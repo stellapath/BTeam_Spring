9c421558bda97a15cf107ede227b9ec188fc3f4d
stellapath <69282475+stellapath@users.noreply.github.com>
2020-09-10 23:47:34 +0900

    안드로이드 로그인 및 회원가입 구현

--- spring_bteam/src/main/java/com/project/bteam/controller/AndroidController.java
+++ spring_bteam/src/main/java/com/project/bteam/controller/AndroidController.java
@@ -1,23 +1,42 @@
 package com.project.bteam.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.project.bteam.board.BoardServiceImpl;
+import com.project.bteam.common.CommonService;
+import com.project.bteam.user.UserServiceImpl;
+import com.project.bteam.user.UserVO;
 
 @Controller
 public class AndroidController {
 	
+	@Autowired private UserServiceImpl user;
+	@Autowired private BoardServiceImpl board;
+	@Autowired private CommonService common;
+	
 	// 안드로이드 회원가입
-	@RequestMapping("/andSignup")
-	public String androidSignup(HttpServletRequest request, Model model) {
-		return "app/andSignup";
+	@ResponseBody @RequestMapping("/andSignup")
+	public String andSignup(UserVO vo) {
+		int result = user.userSignup(vo); 
+		return Integer.toString(result);
+	}
+	
+	// 회원가입 주소 검색창 요청
+	@RequestMapping("/andAddress")
+	public String andAddress() {
+		return "app/andAddress";
 	}
 	
 	// 안드로이드 로그인
 	@RequestMapping("/andLogin")
-	public String androidLogIn(HttpServletRequest request, Model model) {
+	public String andLogin(UserVO vo, Model model) {
+		UserVO login_info = user.userLogin(vo);
+		model.addAttribute("login_info", login_info);
 		return "app/andLogin";
 	}
 	
