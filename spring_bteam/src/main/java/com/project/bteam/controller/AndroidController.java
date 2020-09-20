84316ed3076b2709964120374001cad73b8e6922
stellapath <69282475+stellapath@users.noreply.github.com>
2020-09-20 22:22:13 +0900

    안드로이드 게시판 업데이트

--- spring_bteam/src/main/java/com/project/bteam/controller/AndroidController.java
+++ spring_bteam/src/main/java/com/project/bteam/controller/AndroidController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.bteam.board.BoardServiceImpl;
+import com.project.bteam.board.BoardVO;
 import com.project.bteam.common.CommonService;
 import com.project.bteam.user.UserServiceImpl;
 import com.project.bteam.user.UserVO;
@@ -21,9 +22,10 @@ public class AndroidController {
 	
 	// 안드로이드 회원가입
 	@ResponseBody @RequestMapping("/andSignup")
-	public String andSignup(UserVO vo) {
+	public int andSignup(UserVO vo) {
+		System.out.println("::andSignUp::");
 		int result = user.userSignup(vo); 
-		return Integer.toString(result);
+		return result;
 	}
 	
 	// 회원가입 주소 검색창 요청
@@ -35,9 +37,20 @@ public class AndroidController {
 	// 안드로이드 로그인
 	@RequestMapping("/andLogin")
 	public String andLogin(UserVO vo, Model model) {
+		System.out.println("::andLogin::");
+		System.out.println(vo.getUser_email());
+		System.out.println(vo.getUser_pw());
 		UserVO login_info = user.userLogin(vo);
 		model.addAttribute("login_info", login_info);
 		return "app/andLogin";
+	}
+	
+	// 안드로이드 게시판 목록 TODO 페이징 처리
+	@RequestMapping("/andBoardList")
+	public String andBoardList(int category, Model model) {
+		System.out.println("::andBoardList::");
+		model.addAttribute("boardList", board.boardList(category));
+		return "app/andBoardList";
 	}
 	
 }
